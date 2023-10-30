76cb2907f8d4d0cdf9562458c73a43814842d2a3
saloxitdinovv <134138858+saloxitdinovv@users.noreply.github.com>
2023-10-30 15:53:04 +0500

    первая фиксация

---
+++ pages/registration/script.js
@@ -0,0 +1,37 @@
+let form = document.forms.reg
+let inputs = document.querySelectorAll('input')
+
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    let error = false;
+
+    inputs.forEach((inp) => {
+        if (inp.value.length === 0) {
+            error = true;
+            inp.classList.add("error");
+        } else {
+            inp.classList.remove("error");
+        }
+    })
+    if (error) {
+        return error
+    } else {
+        submit()
+    }
+
+}
+
+
+function submit() {
+    let user = {};
+
+    let fm = new FormData(form);
+
+    fm.forEach((value, key) => {
+        user[key] = value;
+    });
+    console.log(user);
+
+}
